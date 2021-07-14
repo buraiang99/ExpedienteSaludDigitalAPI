01e26855d869a68540e538ed3a097b215ef03716
JorgeM81 <madrigal.jorge81@yahoo.es>
2021-07-13 21:06:40 -0600

    creacion de models

---
+++ ExpedienteSaludDigitalAPI/Controllers/PacienteController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpedienteSaludDigitalAPI.Controllers
+{
+    public class PacienteController : Controller
+    {
+        public IConfiguration Configuration { get; }
+        public PacienteController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        //metodo para guirse
+        [HttpPost]
+        public IActionResult Registrar(DoctorModel doctorModel)
+        {
+            if (ModelState.IsValid)
+            {
+                string conexionString = Configuration["ConnectionStrings:DB_Connection_Turrialba"];
+                var connection = new SqlConnection(conexionString);
+
+                string sqlQuery = $"exec sp_insertarDoctor @param_CEDULA = '{doctorModel.Cedula}', " +
+                    $"@param_CODIGO_MEDICO = '{doctorModel.CodigoMedico}', " +
+                    $"@param_PASS = '{doctorModel.Pass}', " +
+                    $"@param_NOMBRE = '{doctorModel.Nombre}', @param_APELLIDOS = '{doctorModel.Apellidos}'";
+                using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                {
+                    command.CommandType = CommandType.Text;
+                    connection.Open();
+                    command.ExecuteReader();
+                    connection.Close();
+                };
+            }
+            return View("Index");
+
+            [HttpGet]
+            public object Get()
+            {
+                string conexionString = Configuration["ConnectionStrings:DB_Connection_Turrialba"];
+                var connection = new SqlConnection(conexionString);
+                string sqlQuery = $"exec sp_getAllPacientes";
+                using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                {
+                    command.CommandType = CommandType.Text;
+                    connection.Open();
+                    SqlDataReader productosReader = command.ExecuteReader();
+                    while (productosReader.Read())
+                    {
+                        ProductoModel productoTemp = new ProductoModel();
+                        productoTemp.Id = Int32.Parse(productosReader["id"].ToString());
+                        productoTemp.Nombre = productosReader["nombre"].ToString();
+                        productos.Add(productoTemp);
+                    }
+                    connection.Close();
+                };
+            }
+        }
+    }
+}
---
+++ ExpedienteSaludDigitalAPI/Models/PacienteModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpedienteSaludDigitalAPI.Models
+{
+    public class PacienteModel
+    {
+        public int Cedula { get; set; }
+        public string Nombre { get; set; }
+        public string Edad { get; set; }
+        public string TipoSangre { get; set; }
+        public string EstadoCivil { get; set; }
+        public string Domicilio { get; set; }
+
+    } // fin clase
+} // fin namespace
